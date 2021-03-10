c8f29bdca37713384418748fd6a06e377ad79715
Ninjani <48680156+Ninjani@users.noreply.github.com>
2021-03-10 18:24:17 +0100

    build(version): bump

--- Formula/gooseberry.rb
+++ Formula/gooseberry.rb
@@ -4,9 +4,9 @@
 class Gooseberry < Formula
   desc "A command line utility to generate a knowledge base from Hypothesis annotations"
   homepage "https://github.com/out-of-cheese-error/gooseberry"
-  url "https://github.com/out-of-cheese-error/gooseberry/releases/download/0.5.2/gooseberry-macos.tar.gz"
-  sha256 "5daaa4575e77101171cdb7a87696a475445e6bfdb21999da317409e2529ab4ae"
-  version "0.5.2"
+  url "https://github.com/out-of-cheese-error/gooseberry/releases/download/0.6.0/gooseberry-macos.tar.gz"
+  sha256 "18da56c210eb5eb03a274a9efbe671a8b3f6d7c1d9acabd6e32ec53417aea560"
+  version "0.6.0"
 
   def install
     bin.install "gooseberry"
